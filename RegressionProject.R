db11f841a2e38433b21ed6064f84db614fbb78bb
Natalie Meurer <natalie.a.meurer@gmail.com>
2022-03-10 17:50:14 -0700

    Push updates from the team.

--- RegressionProject.R
+++ RegressionProject.R
@@ -7,7 +7,7 @@ install.packages("zoo")
 library(zoo)
 Sales$Floor = rollapplyr(Sales$eth_amount,list(-1:-20),min,fill=NA)
 
-#Reads and merges in Rarity2 to created Merged 
+#Reads and merges in Rarity2 to create df Merged 
 Rarity2 = read.csv("punk_rarities2.csv")
 Merged = merge(Sales,Rarity2,by.x="token_id",by.y="id")
 
@@ -19,7 +19,7 @@ Merged[is.na(Merged) | Merged == "Inf"] <- NA
 Merged[is.na(Merged) | Merged == "NaN"] <- NA
 MergedClean = Merged[complete.cases(Merged),]
 
-#Turns Punk.Type to binary Gender variable 
+#Turns Punk.Type to binary Gender variable in new df MergedClean 
 lookup = c("Male" = 1, "Female" = 0)
 MergedClean$Gender = lookup[MergedClean$Punk.Type]
 
@@ -40,48 +40,79 @@ test = MergedReal[-dt,]
 reg1 = lm(eth_amount ~ Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + ExchangeRate + Gender,data=train)
 summary(reg1)
 
+#Copy regression coefficients to table 
+install.packages("broom")
+library(broom)
+install.packages("clipr")
+library(clipr)
+install.packages("magrittr")
+library(magrittr)
+
+reg1 = lm(eth_amount ~ Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + ExchangeRate + Gender,data=train) %>%
+  tidy() %>%
+  write_clip()
+
 #Finds RMSE of training data (29.45267)
 sqrt(mean((train$eth_amount - reg1$fitted.values)^2))
 
 #Plots fitted values vs. residuals 
-plot(reg1$fitted.values,reg1$residuals,labels=TRUE)
+plot(reg1$fitted.values,reg1$residuals,labels=TRUE,xlab="Fitted Values",ylab="Residuals",main="Fitted Values vs. Residuals")
 
 #Predicts on test data 
 Predictions = predict(reg1, newdata = test)
-plot(Predictions, test$eth_amount)
+plot(Predictions, test$eth_amount,xlab="Predictions",ylab="Price from Test Data",main="Predictions vs. Test Data")
 
 #Finds RMSE of test data (33.17123) 
 sqrt(mean((test$eth_amount - Predictions)^2))
 
 #Lasso
 library(glmnet)
-XVars = sparse.model.matrix(eth_amount ~ Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + ExchangeRate + Gender, data = train)
+XVars = sparse.model.matrix(eth_amount ~ (Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + ExchangeRate + Gender)^2, data = train)
 set.seed(12345)
 Lasso = cv.glmnet(x = XVars,y = train$eth_amount)
-coef(Lasso,s="lambda.min")!=0 #Excludes Full.Type.rarity, Blemishes.rarity,Noses.rarity, and Ears.rarity
+coef(Lasso,s="lambda.min")!=0 
 sum(coef(Lasso,s="lambda.min")!=0)
 
-XVarsTest = sparse.model.matrix(eth_amount ~ Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + ExchangeRate + Gender, data = test)
+XVarsTest = sparse.model.matrix(eth_amount ~ (Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + ExchangeRate + Gender)^2, data = test)
 PredictLasso = predict(Lasso, newx = XVarsTest, s="lambda.min")
-sqrt(mean((test$eth_amount - PredictLasso)^2)) #33.21209 ??? How is this a higher RMSE than w/o Lasso? 
+sqrt(mean((test$eth_amount - PredictLasso)^2)) #30.95439
 
+plot(PredictLasso, test$eth_amount,xlab="Predictions",ylab="Price from Test Data",main="Predictions vs. Test Data")
 
 #-----------------------REGRESSION ON LOG OF DEPENDENT VARIABLE-----------------------
 
-#Creates new variable that is log of eth_amount
+#Creates new variables that are logs of eth_amount and Floor 
 MergedReal$log_eth_amount = log(MergedReal$eth_amount)
+MergedReal$log_Floor = log(MergedReal$Floor)
+MergedReal$log_ExchangeRate = log(MergedReal$ExchangeRate)
 
-#Creates new df that removes Inf log_eth_amount and splits into training/testing data
-MergedLog = MergedReal[is.finite(MergedReal$log_eth_amount),]
+#Creates new df MergedLog that removes Inf log_eth_amount
+MergedLog = MergedReal[is.finite(MergedReal$log_eth_amount) & is.finite(MergedReal$log_Floor) & is.finite(MergedReal$log_ExchangeRate),]
 
+#Splits MergedLog in training and test data 
 set.seed(12345)
-dt = sample(nrow(MergedLog),0.8*nrow(MergedLog))
-trainlog = MergedLog[dt,]
-testlog = MergedLog[-dt,]
+dtlog = sample(nrow(MergedLog),0.8*nrow(MergedLog))
+trainlog = MergedLog[dtlog,]
+testlog = MergedLog[-dtlog,]
 
 #Runs regression on trainlog with log_eth as dependent variable (R^2 = 0.4103)
-reglog = lm(log_eth_amount ~ Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + ExchangeRate + Gender,data=trainlog)
+reglog = lm(log_eth_amount ~ log_Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + log_ExchangeRate + Gender,data=trainlog)
 summary(reglog)
 
+#Copy regression coefficients to table 
+reglog = lm(log_eth_amount ~ log_Floor + Attribute.Count.rarity + Skin.Tone.rarity + isPersonOfColor + Full.Type.rarity + Hair.rarity + Eyes.rarity + Facial.Hair.rarity + Blemishes.rarity + Neck.Accessory.rarity + Mouth.Prop.rarity + Mouth.rarity + Nose.rarity + Ears.rarity + log_ExchangeRate + Gender,data=trainlog) %>%
+  tidy() %>%
+  write_clip()
+
 #Plots fitted values vs. residuals 
-plot(reglog$fitted.values,reglog$residuals,labels=TRUE)
+plot(reglog$fitted.values,reglog$residuals,labels=TRUE,xlab="Fitted Values",ylab="Residuals",main="Fitted Values vs. Residuals")
+
+#Finds RMSE of training data (51.92751)
+sqrt(mean((trainlog$eth_amount - reglog$fitted.values)^2))
+
+#Predicts on test data 
+PredictionsLog = predict(reglog, newdata = testlog)
+plot(PredictionsLog, testlog$eth_amount,xlab="Predictions",ylab="Price from Test Data",main="Predictions vs. Test Data")
+
+#Finds RMSE of test data (53.73768) 
+sqrt(mean((testlog$eth_amount - PredictionsLog)^2))
